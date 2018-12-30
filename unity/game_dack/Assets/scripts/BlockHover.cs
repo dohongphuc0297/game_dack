903b9446953bc15b7e204fd5bb258245fb14725e
PHUC-PC\dohongphuc <dohongphuc1997@gmail.com>
2018-12-30 15:48:35 +0700

    add cursor

---
+++ unity/game_dack/Assets/scripts/BlockHover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BlockHover : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- unity/game_dack/Assets/scripts/levels/Map_1.cs
+++ unity/game_dack/Assets/scripts/levels/Map_1.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Map_1 : MonoBehaviour
 {
+    public Grid grid;
+    public GameObject Cursor;
     private GameStates currentState;
     private BaseCharacterClass Lyn = new Warrior();
+    string str_collider = "not set";
+    Ray ray;
+    RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
-        currentState = GameStates.Start;
+        currentState = GameStates.PlayerSelectTile;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //hover
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
+        Vector3 pos = new Vector3(coordinate.x, coordinate.y, coordinate.z);
+        //Debug.Log(coordinate);
+        pos.x += 0.5f;
+        pos.y += 0.5f;
+        Cursor.transform.position = pos;
+
+        //mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                str_collider = hit.collider.name;
+            }
+        }
         switch (currentState)
         {
             case GameStates.Start:
@@ -38,5 +61,6 @@ public class Map_1 : MonoBehaviour
 
     void OnGUI()
     {
+        GUILayout.Label(str_collider);
     }
 }
