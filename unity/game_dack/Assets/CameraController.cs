ee635f4bf4d8969a0603b49c07d06e760c909f60
PHUC-PC\dohongphuc <dohongphuc1997@gmail.com>
2018-12-30 11:58:10 +0700

    add script camera controller

---
+++ unity/game_dack/Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }else if (Input.GetKey("a") || Input.mousePosition.x <=  panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }else if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }else if (Input.GetKey("s") || Input.mousePosition.y <=  panBorderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+        
+        transform.position = pos;
+    }
+}
---
+++ unity/game_dack/Assets/scripts/levels/TileMap_1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_1_Tile : MonoBehaviour
+{
+    public TileType[] tileTypes;
+
+    int[,] tiles;
+    public int mapSizeX = 42;
+    public int mapSizeY = 26;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
